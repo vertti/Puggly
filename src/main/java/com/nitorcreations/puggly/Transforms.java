618d1825d1394c1924c0fd05f345e72b3982ad6e
Janne Sinivirta <janne.sinivirta@nitorcreations.com>
2015-06-11 12:32:29 +0300

    use a singleton Gson for some performance gains

--- src/main/java/com/nitorcreations/puggly/Transforms.java
+++ src/main/java/com/nitorcreations/puggly/Transforms.java
@@ -1,21 +1,24 @@
 package com.nitorcreations.puggly;
 
+import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonParser;
 import com.nitorcreations.puggly.domain.tranforms.ExchangeTransform;
 
 public class Transforms {
 
+    private static final Gson gson = new GsonBuilder().setPrettyPrinting().create();
+    
     public static ExchangeTransform prettyPrintJsonRequestBody() {
         return exchange -> {
-            exchange.request.body = new GsonBuilder().setPrettyPrinting().create().toJson(new JsonParser().parse(exchange.request.body));
+            exchange.request.body = gson.toJson(new JsonParser().parse(exchange.request.body));
             return exchange;
         };
     }
 
     public static ExchangeTransform prettyPrintJsonResponseBody() {
         return exchange -> {
-            exchange.response.body = new GsonBuilder().setPrettyPrinting().create().toJson(new JsonParser().parse(exchange.response.body));
+            exchange.response.body = gson.toJson(new JsonParser().parse(exchange.response.body));
             return exchange;
         };
     }
